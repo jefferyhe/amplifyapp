1cc34ab2193259894697f342791738e2dc67970c
put-your-name-here <put-your-email-here>
2022-07-14 22:41:26 -0700

    added fb login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FacebookLoginComponent from './facebooklogin.component';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Kamen</h1>
+        <FacebookLoginComponent />
       </header>
     </div>
   );
 }
+
 
 export default App;
